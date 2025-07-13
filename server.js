0505f7b3e1439b544266313697f72c5e12b82153
kristel <klfpaz@scl.edu.gt>
2025-07-13 09:28:55 -0600

    cambios

--- server.js
+++ server.js
@@ -6,12 +6,24 @@ import partidasRoutes from "./routes/partidas.js";
 dotenv.config();
 
 const app = express();
-app.use(cors());
-app.use(express.json());
 
+
+const corsOptions = {
+  origin: [
+    "https://laberinto-front.onrender.com",
+    "http://127.0.0.1:5501"                  
+  ],
+  methods: ["GET", "POST", "OPTIONS"],
+  allowedHeaders: ["Content-Type"]
+};
+
+app.use(cors(corsOptions));
+app.options("*", cors(corsOptions)); 
+app.use(express.json());
 app.use("/partidas", partidasRoutes);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`✅ Servidor corriendo en el puerto ${PORT}`);
 });
+
